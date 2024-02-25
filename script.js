f2cc9b2843525ea486151d731c765fd3cc62df69
Vyom <vyomdeepansh@gmail.com>
2024-02-26 00:15:37 +0545

    v2

---
+++ script.js
@@ -0,0 +1,17 @@
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('a').forEach(function (link) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Add the fade-out class to the body
+        document.body.classList.add('fade-out');
+
+        // Redirect to the new page after the fade-out effect
+        setTimeout(function () {
+          window.location.href = link.getAttribute('href');
+        }, 300); // Adjust the timeout based on the transition duration
+      });
+    });
+  });
+
+
