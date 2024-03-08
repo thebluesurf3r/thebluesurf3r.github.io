6846d7e582c0dfca67a1736480da27dc43fc8942
Vyom <vyomdeepansh@gmail.com>
2024-03-09 00:23:41 +0545

    1

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-  document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('a').forEach(function (link) {
-      link.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('a').forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
 
-        // Add the fade-out class to the body
-        document.body.classList.add('fade-out');
+      // Add the fade-out class to the body
+      document.body.classList.add('fade-out');
 
-        // Redirect to the new page after the fade-out effect
-        setTimeout(function () {
-          window.location.href = link.getAttribute('href');
-        }, 300); // Adjust the timeout based on the transition duration
-      });
+      // Redirect to the new page after the fade-out effect
+      setTimeout(function () {
+        window.location.href = link.getAttribute('href');
+      }, 300); // Adjust the timeout based on the transition duration
     });
   });
-
-const sidebarLinks = document.querySelectorAll('#sidebar_ul li a');
-
-sidebarLinks.forEach(link => {
-  link.addEventListener('mouseover', () => {
-    const targetSectionId = link.href.split('.html')[0]; // Extract section ID from href
-    const targetElement = document.getElementById(targetSectionId);
-    targetElement.classList.add('highlighted'); // Add 'highlighted' class
-  });
-
-  link.addEventListener('mouseout', () => {
-    const targetSectionId = link.href.split('.html')[0];
-    const targetElement = document.getElementById(targetSectionId);
-    targetElement.classList.remove('highlighted'); // Remove 'highlighted' class
-  });
 });
-
